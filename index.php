b6efb6ca93ddf1b66cef927ae1714159e9086d96
KhaiFaeez <khairulfaeez1510@gmail.com>
2021-10-22 01:48:07 +0800

    vision

--- index.php
+++ index.php
@@ -36,7 +36,7 @@
         <h1 class="big-name text-end">JDJ</h1>
       </div>
       <div class="col-md-8">
-        <h1 class="mb-0">xTECHNOLOGY (M) SDN. BHD. <small class="subtitle">(1359495-U)</small></h1>
+        <h1 class="mb-0">TECHNOLOGY (M) SDN. BHD. <small class="subtitle">(1359495-U)</small></h1>
         <h1 class="pt-0 second-name">METAL TRADING. <small class="subtitle">(002687917-T)</small></h1>
         <hr class="float-end name-underline">
       </div>
@@ -89,70 +89,18 @@
     </div>
   </header><!-- End Header -->
   <br>
-  <!-- ======= Hero Section ======= -->
-  <section id="hero">
-    <div id="heroCarousel" data-bs-interval="5000" class="carousel slide carousel-fade" data-bs-ride="carousel">
-
-      <ol class="carousel-indicators" id="hero-carousel-indicators"></ol>
-
-      <div class="carousel-inner" role="listbox">
-
-        <!-- Slide 1 -->
-        <div class="carousel-item active" style="background-image: url(assets/img/slide/slide-1.jpg)">
-          <div class="carousel-container">
-            <div class="container">
-              <h2 class="animate__animated animate__fadeInDown">Welcome to <span>Sailor</span></h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-            </div>
-          </div>
-        </div>
-
-        <!-- Slide 2 -->
-        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-2.jpg)">
-          <div class="carousel-container">
-            <div class="container">
-              <h2 class="animate__animated animate__fadeInDown">Lorem Ipsum Dolor</h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-            </div>
-          </div>
-        </div>
-
-        <!-- Slide 3 -->
-        <div class="carousel-item" style="background-image: url(assets/img/slide/slide-3.jpg)">
-          <div class="carousel-container">
-            <div class="container">
-              <h2 class="animate__animated animate__fadeInDown">Sequi ea ut et est quaerat</h2>
-              <p class="animate__animated animate__fadeInUp">Ut velit est quam dolor ad a aliquid qui aliquid. Sequi ea ut et est quaerat sequi nihil ut aliquam. Occaecati alias dolorem mollitia ut. Similique ea voluptatem. Esse doloremque accusamus repellendus deleniti vel. Minus et tempore modi architecto.</p>
-              <a href="#about" class="btn-get-started animate__animated animate__fadeInUp scrollto">Read More</a>
-            </div>
-          </div>
-        </div>
-
-      </div>
-
-      <a class="carousel-control-prev" href="#heroCarousel" role="button" data-bs-slide="prev">
-        <span class="carousel-control-prev-icon bi bi-chevron-left" aria-hidden="true"></span>
-      </a>
-
-      <a class="carousel-control-next" href="#heroCarousel" role="button" data-bs-slide="next">
-        <span class="carousel-control-next-icon bi bi-chevron-right" aria-hidden="true"></span>
-      </a>
-
-    </div>
-  </section><!-- End Hero -->
+  
 
   <main id="main">
     <?php 
 
       /*get content*/
-      if(empty($module)){
+      if(!isset($_REQUEST['module'])){
         echo file_get_contents("home.php");
       }
       else
       {
-        echo file_get_contents($module.".php"); 
+        echo file_get_contents($_REQUEST['module'].".php"); 
       }
     ?>
     
---
+++ vision.php
@@ -0,0 +1,48 @@
+<section id="about" class="about">
+      <div class="container">
+
+      <div class="section-title">         
+          <p>Vision and mission</p>
+          <hr class="footerline">
+        </div>
+        
+   
+
+        <div class="row content">
+        
+          <div class="col-lg-12 ">
+          <h4>VISION</h4>
+            <p>
+            <font class="yellow">JDJ METAL TRADING</font>	HAS	CREATED	OUR	VISION	IS EMBEDDED IN	OUR	COMMITMENT TO ACTIVELY	PURSUE	AND ABIDE BY OUR BELIEFS:
+            </p>
+            <ul>
+              <li><i class="ri-check-double-line"></i> TO PROVIDE	EFFICIENCY	AND EFFECTIVENESS IN MANAGEMENT AND DELIVERANCE OF SERVICES.
+</li>
+              <li><i class="ri-check-double-line"></i> TO	ACHIEVE	CORPORATE	BRANDING	AS	BEING	A BUSINESS ENTITY	THAT	BALANCES	EXCELLENT	SERVICES	AND BUSINESS	INTEGRITY.
+</li>
+              <li><i class="ri-check-double-line"></i> TO  HAVE  MUTUALLY  REWARDING  PARTNERSHIPS  BASED ON  RESPECT	AND	SUPPORT	FOR  THE	COMMUNITY,  EMPLOYEES,, CUSTOMERS,	LIVING  ENVIRONMENT	AND	THE GENERAL PUBLIC.
+</li>
+            </ul>
+           
+          </div>
+
+          <div class="col-lg-12 ">
+          <h4>MISSION</h4>
+            <p>
+            OUR MISSION IS TO BE ONE OF THE MOST DEDICATED, RELIABLE, PROSPEROUS, EFFECTIVE AND SUCCESSFUL COMPANY IN THE SCRAP METAL TRADING ANDv RECYCLE CONTRACTOR AND SUB-CONTRACTORS TO CONDUCT THE JOBS THAT ARE PROVIDING SERVICE SUCH AS DISMANTLING SERVICE, SCRAP CLEAR-UP SERVICE, CO-COPERATING	TO MANAGE THE SCRAP OUTCOMES IN FACTORIES AND MANY MORE.
+
+
+
+
+
+ 
+
+            </p>
+            
+           
+          </div>
+
+        </div>
+
+      </div>
+    </section>
